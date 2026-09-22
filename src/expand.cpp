7d9111baf22e8de4812db29e1a69468639baa526
liammmax <99236788+liammaxp@users.noreply.github.com>
2026-09-21 22:21:29 -0600

    finish

--- src/expand.cpp
+++ src/expand.cpp
@@ -5,10 +5,26 @@
 // Expands the binary representation of input by a factor of scale.
 // e.g., expand(0b1111ull, 3) == 0b001001001001
 uint64_t expand(uint64_t input, uint32_t scale) {
-    return 0;  // replace this with your impl!
+    assert(scale >= 1);
+    uint64_t result = 0;
+    for (uint32_t i = 0; i < 64; ++i){
+        if (static_cast<uint64_t>(i) * scale >=64){
+            break;
+        }
+        if (input & (1ULL << i)){
+            result |= (1ULL << (i * scale));
+        }
+    }
+    return result;  // replace this with your impl!
 }
 
 int main() {
+    assert(expand(0b1111, 3) == 0b001001001001);
+    assert(expand(0b0101, 2) == 0b00010001);
+    assert(expand(0b101101, 1) == 0b101101);
+    assert(expand(0, 5) == 0);
+    assert(expand(0, 5) == 0);
+    assert(expand(0b11, 64) == 0b1);
     // test here...
     return 0;
 }
--- src/list_vs_array.cpp
+++ src/list_vs_array.cpp
@@ -1,10 +1,63 @@
 #include <cstddef>  // size_t
 
 #include "timer.h"
+#include <cstdint>
+#include <iostream>
+#include <list>
+#include <random>
+#include <vector>
 
 constexpr size_t SIZE = 16000000;
 
 int main() {
+    std::mt19937_64 rng(0);
+    std::list<uint64_t> values;
+    Timer timer;
+    for (size_t i = 0; i < SIZE; ++i){
+        values.push_back(rng());
+    }
+
+    auto listInsertTime = timer.click<Timer::Micros>();
+    std::cout << listInsertTime << '\n';
+    rng.seed(0);
+    std::vector<uint64_t> vectorValues;
+
+    Timer timer2;
+    for (size_t i = 0; i < SIZE; ++i){
+        vectorValues.push_back(rng());
+    }
+    auto vectorTime = timer2.click<Timer::Micros>();
+    rng.seed(0);
+    std::vector<uint64_t> reservedVector;
+    reservedVector.reserve(SIZE);
+
+    Timer timer3;
+    for (size_t i = 0; i < SIZE; ++i){
+        reservedVector.push_back(rng());
+    }
+    auto reservedVectorTime = timer3.click<Timer::Micros>();
+
+    Timer sumTimer;
+    uint64_t sum = 0;
+    for (uint64_t value : values){
+        sum += value;
+    }
+    auto sumTime = sumTimer.click<Timer::Micros>();
+    std::cout << sum << '\n';
+    std::cout << sumTime << '\n';
+
+    sum = 0;
+
+    Timer vectorSumTimer;
+    for (uint64_t value : reservedVector){
+        sum += value;
+    }
+    auto vectorSumTime = vectorSumTimer.click<Timer::Micros>();
+    std::cout << sum << '\n';
+    std::cout << vectorSumTime << '\n';
+    std::cout << listInsertTime << '\n';
+    std::cout << vectorTime << '\n';
+    std::cout << reservedVectorTime << '\n';
     // test here...
     return 0;
 }
